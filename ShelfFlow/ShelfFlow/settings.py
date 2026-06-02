80e6f2ba23291a253419fef42c0f68cb6fc8c284
Shahab <sh12khoshnevis@gmail.com>
2026-06-02 14:46:10 +0330

    Chore: create project apps.

--- ShelfFlow/ShelfFlow/settings.py
+++ ShelfFlow/ShelfFlow/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'books',
+    'borrowing',
+    'dashboard',
+    'members',
+
 ]
 
 MIDDLEWARE = [
