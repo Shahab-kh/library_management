780eb4417143d3a60d10719c28cd0eab498a75ed
Shahab <sh12khoshnevis@gmail.com>
2026-08-16 17:49:34 +0330

    feat: add book list page and search functionality

--- ShelfFlow/books/views.py
+++ ShelfFlow/books/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+    query = request.GET.get("q")
+    books = Book.objects.all()
+
+    if query :
+        books = Book.objects.filter(
+            Q(title__icontains=query) or
+            Q(author__icontains=query) 
+        )
+
+    return render(request, "books/book_list.html", { "books" : books })
+
+# books/book_list.html
